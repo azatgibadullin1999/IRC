7f10fbc21c6e18985001ebacaeb7f6ae5c0c05d2
zera <svetik.avtonomov@gmail.com>
2022-01-08 16:00:45 +0300

    Base for ServerClientService

--- Server/ServerClientService.hpp
+++ Server/ServerClientService.hpp
@@ -6,7 +6,7 @@
 /*   By: zera <zera@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 10:22:23 by zera              #+#    #+#             */
-/*   Updated: 2022/01/06 20:09:05 by zera             ###   ########.fr       */
+/*   Updated: 2022/01/08 16:00:00 by zera             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 
 # include <vector>
 # include "ServerModels/ServerClient.hpp"
+# include "../models/models.hpp"
 
 
 class ServerClientService
@@ -25,10 +26,20 @@ class ServerClientService
 		ServerClientService(void);
 		~ServerClientService(void);
 
-		
+		void	addRequest(int socket, ServerMessage *request);
+
+		void	addResponse(ServerMessage *response);
+
+		bool	needResponse(ClientRequest &request);
+
+		bool	needResponse(ServerMessage &request);
+
+		int		sendRequest(ServerMessage &request);
 
 	private:
 		std::vector<ServerClient>		_serverClients;
+
+		void	_execudeRequest(ServerClient &serverClient, ServerMessage *request);
 };
 
 
--- models/ServerMessage.cpp
+++ models/ServerMessage.cpp
@@ -3,43 +3,70 @@
 /*                                                        :::      ::::::::   */
 /*   ServerMessage.cpp                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: zera <zera@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 21:40:47 by root              #+#    #+#             */
-/*   Updated: 2022/01/07 17:24:52 by root             ###   ########.fr       */
+/*   Updated: 2022/01/08 13:15:51 by zera             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "ServerMessage.hpp"
 
 ServerMessage::ServerMessage(const std::vector<std::string> &requestData, const Commands::ServerCommandType type) :
-	_requestData(requestData), _uid(getStringUID()), _type(type) { }
+	_requestData(requestData), _uid(getStringUID()), _type(type), _waitingResponses(0) { }
 
 ServerMessage::~ServerMessage() { }
 
-const std::string		&ServerMessage::getPassword() const {
+const Commands::ServerCommandType	&ServerMessage::getType() const {
+	return _type;
+}
+
+
+const std::string					&ServerMessage::getPassword() const {
 	__checkArguments(PASSWORD);
 	return _requestData[PASSWORD];
 }
 
-const std::string		&ServerMessage::getServerCommand() const {
+const std::string					&ServerMessage::getServerCommand() const {
 	__checkArguments(SERVER_COMMAND);
 	return _requestData[SERVER_COMMAND];
 }
 
-const std::string		&ServerMessage::getClientArgs() const {
+const std::string					&ServerMessage::getClientArgs() const {
 	__checkArguments(CLIENT_ARGS);
 	return _requestData[CLIENT_ARGS];
 }
 
-const std::string		&ServerMessage::getStringUID() const {
+const std::string					&ServerMessage::getStringUID() const {
 	__checkArguments(CLIENT_ARGS);
 	return _requestData[STR_UID];
 }
 
-const UID				&ServerMessage::getUID() const {
+const UID							&ServerMessage::getUID() const {
 	return _uid;
 }
+
+std::vector<ServerMessage*>			&ServerMessage::getResponses() {
+	return _responses;
+}
+
+
+void								ServerMessage::setUID(UID uid) {
+	_uid = uid;
+}
+
+void								ServerMessage::setWaitingResponses(int waitingResponses) {
+	_waitingResponses = waitingResponses;
+}
+
+bool								ServerMessage::addResponse(ServerMessage *response) {
+	_responses.push_back(response);
+	if (--_waitingResponses == 0) {
+		return (true);
+	}
+	return (false);
+}
+
 
 void		ServerMessage::__checkArguments(ArgumentNumber n) const {
 	if (_requestData.size() <= n)
