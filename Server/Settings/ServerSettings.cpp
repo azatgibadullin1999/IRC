54d7d29c6c863b2b47055b8d8cd5671610279e23
zera <svetik.avtonomov@gmail.com>
2022-01-11 01:23:08 +0300

    ServerSettings

---
+++ Server/Settings/ServerSettings.cpp
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ServerSettings.cpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zera <zera@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/10 11:41:00 by zera              #+#    #+#             */
+/*   Updated: 2022/01/11 01:06:00 by zera             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ServerSettings.hpp"
+
+ServerSettings::ServerSettings(const std::string port, const std::string password) {
+	_connection = false;
+	_port = port;
+	_password = password;
+}
+
+ServerSettings::ServerSettings(const std::string connection, const std::string port,
+					const std::string password) throw(ServerSettingsException) {
+	_connection = true;
+	_parseConnection(connection);
+	_port = port;
+	_password = password;
+
+	std::cout << "Host \"" << _hostConnection << "\"\nPort \"" << _portConnection << "\"\nPassword \"" << _passwordConnection <<
+		"\"\n-------------\nPort \"" << _port << "\"\nPassword \"" << _password << "\"" << std::endl;
+}
+
+void			ServerSettings::_parseConnection(const std::string & connection) {
+	// std::cout << connection << std::endl;
+	if (*connection.begin() != '[')
+		throw (ServerSettingsException("Connection must begin with ["));
+	if (*(--connection.end()) != ']')
+		throw (ServerSettingsException("Connection must ends with ] "));
+	size_t hostEnd = connection.find(':');
+	_hostConnection.insert(0, &connection[1], hostEnd - 1);
+
+	size_t portStart = hostEnd + 1;
+	size_t portEnd = connection.find(':', portStart);
+	_portConnection = atoll(&connection[portStart]);
+
+	size_t passwordStart = portEnd + 1;
+	size_t passwordEnd = connection.find(']');
+	_passwordConnection.insert(0, &connection[passwordStart], passwordEnd - passwordStart);
+}
+
+std::string		ServerSettings::getPort() const {
+	return _port;
+}
+
+std::string		ServerSettings::getPassword() const {
+	return _password;
+}
+
+bool			ServerSettings::haveConnection() const {
+	return _connection;
+}
+
+std::string		ServerSettings::getHostConnection() const {
+	return _hostConnection;
+}
+
+int				ServerSettings::getPortConnection() const {
+	return _portConnection;
+}
+
+std::string		ServerSettings::getPasswordConnection() const {
+	return _password;
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include "Server/Server.hpp"
+#include "Server/Settings/ServerSettings.hpp"
 
-int main() {
+int main(int ac, char **av) {
+ServerSettings *settings;
+
+	if (ac == 3) {
+		try {
+			settings = new ServerSettings(av[1], av[2]);
+		} catch (ServerSettingsException &e) {
+			std::cout << e.what() << std::endl;
+		}
+	} else if (ac == 4) {
+		try {
+			settings = new ServerSettings(av[1], av[2], av[3]);
+		} catch (ServerSettingsException &e) {
+			std::cout << e.what() << std::endl;
+		}
+	} else {
+		printf("Error");
+		exit(-1);
+	}
+
 	Server server = Server();
 
 	server.run();
