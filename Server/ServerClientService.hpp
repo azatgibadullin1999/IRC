a63db8808927f459d222ae9f7cbffd918469c398
zera <svetik.avtonomov@gmail.com>
2022-01-16 14:50:17 +0300

    Implemented Client, Server and Connection services to mainCycle

--- Server/ServerClientService.hpp
+++ Server/ServerClientService.hpp
@@ -6,7 +6,7 @@
 /*   By: zera <zera@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 10:22:23 by zera              #+#    #+#             */
-/*   Updated: 2022/01/14 15:56:19 by zera             ###   ########.fr       */
+/*   Updated: 2022/01/16 10:53:51 by zera             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 # include <sys/socket.h>
 # include "ServerModels/ServerClient.hpp"
 # include "../models/models.hpp"
+# include "../models/Response.hpp"
 
 
 class ServerClientService
@@ -30,20 +31,30 @@ class ServerClientService
 
 		void	addServerClient(int socket);
 
-		void	addRequest(int socket, ServerMessage *request);
+		void		sendConnectionRequest(int socket, ServerMessage *request);
 
-		void	addResponse(ServerMessage *response);
+		Response	*addRequest(int socket, ServerMessage *request, Response *response);
 
-		bool	needResponse(ClientRequest &request);
+		void		addConnectionRequest(int socket, ServerMessage *request);
 
-		bool	needResponse(ServerMessage &request);
+		Response	*addResponse(ServerMessage *request);
 
-		int		sendRequest(int socket);
+		void	inBoxMsg(int socket, ServerMessage *serverMessage);
+
+		void	sendServerMsg(int socket);
+
+		void	setFds(fd_set &writeFds);
 
 	private:
 		std::vector<ServerClient*>		_serverClients;
 
-		void	_execudeRequest(ServerClient &serverClient, ServerMessage *request);
+		std::vector<ServerMessage*>		_ourMessages;
+
+		ServerClient		*_getServerClient(int socket);
+
+		Response			*_execudeRequest(std::vector<ServerMessage*>::iterator &request);
+
+		void				_execudeRequest(ServerClient *serverClient, std::vector<ServerMessage*>::iterator &request);
 };
 
 
--- Server/ServerModels/ServerClient.hpp
+++ Server/ServerModels/ServerClient.hpp
@@ -6,7 +6,7 @@
 /*   By: zera <zera@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 19:19:48 by zera              #+#    #+#             */
-/*   Updated: 2022/01/14 15:56:07 by zera             ###   ########.fr       */
+/*   Updated: 2022/01/15 22:00:55 by zera             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,11 +29,18 @@ class ServerClient
 
 		int								getSocket() { return _socket; }
 
-		std::vector<ServerMessage*>&	getRequests() { return _requests; }
+		std::vector<ServerMessage*>*	getRequests() { return &_requests; }
 
+		std::vector<ServerMessage*>*	getMsgToSend() { return &_msgToSend; }
+
+		std::string						getPassword() { return _password; }
+
+		void							setPassword(std::string password) { _password = password; }
 	private:
 		int							_socket;
 		std::vector<ServerMessage*>	_requests;
+		std::vector<ServerMessage*>	_msgToSend;
+		std::string					_password;
 };
 
 #endif
